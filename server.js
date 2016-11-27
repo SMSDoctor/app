256253d82610d13d813ef8c3340255f8a8bf02db
Kaythay <kathleenxw@gmail.com>
2016-11-26 19:10:38 -0500

    Adding basic js and html files.

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+//creating instance of the server
+http.createServer(function(request, response){
+    var path = url.parse(request.url).pathname;
+    fs.readFile(path.substr(1), function(err,data){
+        if (err){
+           //print error page
+           response.writeHead(404, {'Content-Type': 'text/html'});
+        } else {
+          //print html page
+           response.writeHead(200, {'Content-Type': 'text/html'});
+           response.write(data.toString());
+        }
+        response.end();
+    });
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
